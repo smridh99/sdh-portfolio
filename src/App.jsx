f52e90e835bb363e5b0af897c5ffd3be23b58e55
Smridh A <smridh@v1040-wn-rt-b-42-234.campus-dynamic.uwaterloo.ca>
2023-07-27 18:08:41 -0400

    Updated Browser Route in App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -18,7 +18,7 @@ function App() {
       <ThemeProvider theme={darkMode.value ? darkTheme : lightTheme}>
         <GlobalStyles />
         <div className="App">
-          <BrowserRouter>
+          <BrowserRouter basename={process.env.PUBLIC_URL}>
             <MainApp />
           </BrowserRouter>
         </div>
